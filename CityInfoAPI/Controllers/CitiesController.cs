c12a47cd8f9f1b42825dbf2297dd0eebfb3e9cb8
pagawde <pagawde@cisco.com>
2022-05-23 19:49:15 +0530

    Add project files.

---
+++ CityInfoAPI/Controllers/CitiesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfoAPI.Controllers
+{
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new { id = 1,  Name = "Mumbai"},
+                    new { id = 2, Name = "Bengaluru"}
+                });
+        }
+    }
+}
